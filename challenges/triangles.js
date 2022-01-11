072cd639b3f3a27340a9f0430f01a72c08fddea3
Mohamad EL-Chanti <m.elchanti@gmail.com>
2022-01-11 12:35:29 -0800

    started the challenges

--- challenges/triangles.js
+++ challenges/triangles.js
@@ -1,12 +1,15 @@
+"use strict";
+
 /*
 Problem: Identify the type of triangle
   Input: 3 numbers
-  Output: 1 string
+  Output: 1 object
+  
 Examples:
-  Input: triangle(3, 4, 5)
+  Input: new Triangle(2, 2, 2)
   Output: scalene
 
-  Input: triangle(3, 3, 5)
+  Input: new Triangle(3, 3, 5)
   output: isosceles
 
   Input: triangle(3, 3, 3)
@@ -19,14 +22,18 @@ Examples:
   output: invalid
 
 Data structures:
-  put them in an array and solve
+  put the sides in an array and solve
+  return an object with one method
 
 Algorithm:
 
+  SET Triangle = {
+    constructor()
+  }
   SET sides = [side1, side2, side3]
   loop through the sides to find out if one of them is not a number
     return invalid
-  
+
   sort the sides in order
 
   find out if the sum of the two shorter sides is shorter than the third
@@ -34,49 +41,57 @@ Algorithm:
 
   loop through the sides to find out if all 3 are equal
     return equilateral
-  
+
   loop through the sides to find out if two of them are equal
     return isosceles
 */
 
-function invalidEntry(sides) {
-  return sides.some(side =>  isNaN(Number(side)));
-}
+class Triangle {
+  constructor(side1, side2, side3) {
 
-function invalidTriangle(sides) {
-  return (sides[0] + sides[1]) < sides[2];
-}
+    this.sides = [side1, side2, side3];
 
-function isoscelesTriangle(sides) {
-  return sides.some((side, index) => side === sides[index + 1]);
-}
+    if (this.invalidEntry()) {
+      throw new Error(TypeError);
+    }
 
-function equilateralTriangle(sides) {
-  return (sides[0] === sides[1]) && (sides[0] === sides[2]);
-}
+    this.sides.sort((a, b) => a - b);
 
-function triangle(side1, side2, side3) {
-  let sides = [side1, side2, side3];
-  if (invalidEntry(sides)) {
-    return 'invalid triangle';
+    if (this.invalidTriangle()) {
+      throw new Error(TypeError);
+    }
   }
 
-  sides.sort((a, b) => a - b);
+  kind() {
+    if (this.equilateralTriangle()) {
+      return 'equilateral';
+    }
 
-  if (invalidTriangle(sides)) {
-    return 'invalid triangle';
+    if (this.isoscelesTriangle()) {
+      return 'isosceles';
+    }
+
+    return 'scalene';
   }
 
-  if (equilateralTriangle(sides)) {
-    return 'equilateral triangle';
+  invalidEntry() {
+    return this.sides.some(side =>  isNaN(Number(side)) || side <= 0);
   }
 
-  if (isoscelesTriangle(sides)) {
-    return 'isosceles triangle';
+  invalidTriangle() {
+    return (this.sides[0] + this.sides[1]) <= this.sides[2];
+
   }
 
-  return 'scalene triangle';
+  equilateralTriangle() {
+    return (this.sides[0] === this.sides[1]) &&
+      (this.sides[0] === this.sides[2]);
+  }
+
+  isoscelesTriangle() {
+    return this.sides.some((side, index) => side === this.sides[index + 1]);
+  }
 
 }
 
-module.exports = triangle;
+module.exports = Triangle;
--- challenges/triangles.test.js
+++ challenges/triangles.test.js
@@ -13,105 +13,80 @@ output: invalid
 Input: triangle (s, 3, 4)
 output: invalid*/
 
-let Triangle = require('./triangles');
-
-describe('test the triangle class', () => {
-  test('test isosceles', () => {
-    expect(Triangle(3, 3, 5)).toBe('isosceles triangle');
-  });
-
-  test('test scalene', () => {
-    expect(Triangle(3, 4, 5)).toBe('scalene triangle');
-  });
-
-  test('test equilateral', () => {
-    expect(Triangle(3, 3, 3)).toBe('equilateral triangle');
-  });
-
-  test('invalid triangle', () => {
-    expect(Triangle(3, 3, 7)).toBe('invalid triangle');
-  });
-
-  test('invalid triangle', () => {
-    expect(Triangle('s', 3, 4)).toBe('invalid triangle');
-  });
-});
-
-
-
+let Triangle = require('./triangles.js');
 
 describe("Triangle", () => {
   test("equilateral triangles have equal sides", () => {
     const triangle = new Triangle(2, 2, 2);
-    expect(Triangle(2, 2, 2)).toEqual("equilateral triangle");
+    expect(triangle.kind()).toEqual("equilateral");
   });
 
-  xtest("larger equilateral triangles also have equal sides", () => {
+  test("larger equilateral triangles also have equal sides", () => {
     const triangle = new Triangle(10, 10, 10);
-    expect(triangle.kind()).toEqual("equilateral triangle");
+    expect(triangle.kind()).toEqual("equilateral");
   });
 
-  xtest("isosceles triangles have last two sides equal", () => {
+  test("isosceles triangles have last two sides equal", () => {
     const triangle = new Triangle(3, 4, 4);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  xtest("isosceles trianges have first and last sides equal", () => {
+  test("isosceles trianges have first and last sides equal", () => {
     const triangle = new Triangle(4, 3, 4);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  xtest("isosceles triangles have two first sides equal", () => {
+  test("isosceles triangles have two first sides equal", () => {
     const triangle = new Triangle(4, 4, 3);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  xtest("isosceles triangles have in fact exactly two sides equal", () => {
+  test("isosceles triangles have in fact exactly two sides equal", () => {
     const triangle = new Triangle(10, 10, 2);
     expect(triangle.kind()).toEqual("isosceles");
   });
 
-  xtest("scalene triangles have no equal sides", () => {
+  test("scalene triangles have no equal sides", () => {
     const triangle = new Triangle(3, 4, 5);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  xtest("scalene triangles have no equal sides at a larger scale too", () => {
+  test("scalene triangles have no equal sides at a larger scale too", () => {
     const triangle = new Triangle(10, 11, 12);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  xtest("scalene triangles have no equal sides in descending order either", () => {
+  test("scalene triangles have no equal sides in descending order either", () => {
     const triangle = new Triangle(5, 4, 2);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  xtest("very small triangles are legal", () => {
+  test("very small triangles are legal", () => {
     const triangle = new Triangle(0.4, 0.6, 0.3);
     expect(triangle.kind()).toEqual("scalene");
   });
 
-  xtest("test triangles with no size are illegal", () => {
+  test("test triangles with no size are illegal", () => {
     expect(() => { new Triangle(0, 0, 0) }).toThrow();
   });
 
-  xtest("triangles with negative sides are illegal", () => {
+  test("triangles with negative sides are illegal", () => {
     expect(() => { new Triangle(3, 4, -5) }).toThrow();
   });
 
-  xtest("triangles violating triangle inequality are illegal", () => {
+  test("triangles violating triangle inequality are illegal", () => {
     expect(() => { new Triangle(1, 1, 3) }).toThrow();
   });
 
-  xtest("triangles violating triangle inequality are illegal 2", () => {
+  test("triangles violating triangle inequality are illegal 2", () => {
     expect(() => { new Triangle(7, 3, 2) }).toThrow();
   });
 
-  xtest("triangles violating triangle inequality are illegal 3", () => {
+  test("triangles violating triangle inequality are illegal 3", () => {
     expect(() => { new Triangle(10, 1, 3) }).toThrow();
   });
 
-  xtest("triangles violating triangle inequality are illegal 4", () => {
+  test("triangles violating triangle inequality are illegal 4", () => {
     expect(() => { new Triangle(1, 1, 2) }).toThrow();
   });
 });
